687fbf448ab629b585c33a14f73ce5abb5691114
Tarokh Baniadam <tarokh.bani@gmail.com>
2026-09-17 01:27:39 -0400

    Add files via upload

---
+++ zr-orders-s4gsie1j/config.js
@@ -0,0 +1,6 @@
+// Orders backend endpoint. Paste the Apps Script web app URL (it ends in /exec)
+// between the quotes, exactly like RSVP_ENDPOINT in rsvp.html. While it is empty
+// the app talks to the local mock backend on localhost only.
+window.__ORDERS_CONFIG__ = {
+  endpoint: "https://script.google.com/macros/s/AKfycbzsxx-JC1x7bJLFk0s3fiAdLjA7CrkN_O2Fkay5clWNTXc120rbfAmcyNRjY4o4WHMy/exec",
+};
